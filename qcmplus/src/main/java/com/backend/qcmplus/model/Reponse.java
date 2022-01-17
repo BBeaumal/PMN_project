88c5bf9da78d2cbf37df309188dc28c04671edad
Bastien <bastienreturn@outlook.fr>
2022-01-17 16:50:26 +0100

    Fix modele

--- qcmplus/src/main/java/com/backend/qcmplus/model/Reponse.java
+++ qcmplus/src/main/java/com/backend/qcmplus/model/Reponse.java
@@ -3,6 +3,8 @@ package com.backend.qcmplus.model;
 import java.io.Serializable;
 
 import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -17,6 +19,7 @@ public class Reponse implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long idReponse;
 
+    @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "id_question")
     private Question question;
