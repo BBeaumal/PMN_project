b8e4f69eafadb2bd7c752e4c227406e771665e7a
Baptiste Beaumal <baptiste.beaumal@gmail.com>
2022-01-25 11:03:27 +0100

    Creation du bean Parcours

---
+++ qcmplus/src/main/java/com/backend/qcmplus/model/ParcoursBean.java
@@ -0,0 +1,20 @@
+package com.backend.qcmplus.model;
+
+import java.util.Date;
+
+
+public class ParcoursBean  {
+
+    private Long idParcours;
+    private int numeroTentative;
+    private Date dateFin;
+    private Date dateRealisation;
+    private int note;
+
+    private int reponse;
+
+   private Long idUtilisateur;
+
+    private Long idQuestion;
+
+ }
