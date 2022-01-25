e427e6dad2be1e1e7bedd3f827ff38f182c795a3
Baptiste Beaumal <baptiste.beaumal@gmail.com>
2022-01-25 11:03:03 +0100

    Ajout de la méthode pour noter un questionnaire

--- qcmplus/src/main/java/com/backend/qcmplus/controller/UserController.java
+++ qcmplus/src/main/java/com/backend/qcmplus/controller/UserController.java
@@ -2,14 +2,19 @@ package com.backend.qcmplus.controller;
 
 import com.backend.qcmplus.model.Question;
 import com.backend.qcmplus.model.Questionnaire;
-import com.backend.qcmplus.model.Utilisateur;
+import com.backend.qcmplus.model.ReponseUtilisateurQuestion;
 import com.backend.qcmplus.service.QuestionService;
 import com.backend.qcmplus.service.QuestionnaireService;
+import com.backend.qcmplus.service.ReponseUtilisateurQuestionService;
 import com.backend.qcmplus.service.UtilisateurService;
 import com.backend.qcmplus.utils.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
@@ -21,14 +26,20 @@ import java.util.Optional;
 public class UserController {
 
     @Autowired
+    private UtilisateurService utilisateurService;
+
+    @Autowired
     private QuestionnaireService surveyService;
 
     @Autowired
     private QuestionService questionService;
 
+    @Autowired
+    private ReponseUtilisateurQuestionService reponseUtilisateurQuestionService;
+
     @GetMapping("/login")
-    Mono<String> hello(){
-       return Mono.just("loged");
+    Mono<String> hello() {
+        return Mono.just("loged");
     }
 
     // Find All Survey
@@ -60,4 +71,45 @@ public class UserController {
             throw new UserNotFoundException(id);
         return Mono.just(foundQuestion.get());
     }
+
+    @GetMapping("/questionnaire/{idQuestionnaire}/parcours")
+    public double noteOneQuestionnaire(@PathVariable Long idQuestionnaire) {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        List<ReponseUtilisateurQuestion> listeQuestionsRepondues = reponseUtilisateurQuestionService.listAllReponsesFromLastTentative(idQuestionnaire);
+        double note = 0;
+
+        if (principal instanceof UserDetails) {
+            for (ReponseUtilisateurQuestion reponseUtilisateurQuestion : listeQuestionsRepondues) {
+                if (reponseUtilisateurQuestion.getReponse() == 1) {
+                    note++;
+                }
+            }
+            note=(note / listeQuestionsRepondues.size()) * 20;
+            return note;
+        }
+        return 0;
+    }
+
+/*    Mono<List<ReponseUtilisateurQuestion>> findAllReponses(@PathVariable Long id) throws UserNotFoundException {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof UserDetails) {
+            String username = ((UserDetails)principal).getUsername();
+            System.out.println("Utilisateur : "+username);
+            Utilisateur tempUser = utilisateurService.getUtilisateurByLogin(username);
+            if ( tempUser == null )
+                System.out.println("User not found with id survey");
+            else{
+                if (tempUser.isIsadmin())
+                    System.out.println("User must not be Admin");
+                else{
+                    System.out.println("Utilisateur : "+tempUser.getIdUtilisateur()+ "  : "+tempUser.getPrenom());
+                    return Mono.just(reponseUtilisateurQuestionService.listAllQuestionsByUtilisateur(tempUser.getIdUtilisateur()));
+                }
+            }
+        } else {
+          throw new UserNotFoundException(id);
+        }
+        return Mono.just(reponseUtilisateurQuestionService.listAllQuestionsByUtilisateur(id));
+    }*/
+
 }
